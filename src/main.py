826938c6f638b73d13662c94b6d6a9a7894c35c3
aarjookhand <03aarjoo@gmail.com>
2026-07-09 22:31:40 +0200

    Add raw csv and data extraction

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 from src.database import test_connection
+from src.extract import extract_customer_data
 
 if __name__ == "__main__":
     test_connection()
+
+    df = extract_customer_data()
+    print("CSV data extracted successfully.")
+    print(f"Rows: {len(df)}")
+    print(f"Columns: {len(df.columns)}")
